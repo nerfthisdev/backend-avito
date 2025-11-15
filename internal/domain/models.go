73ece45a15b89bdc65d48bacf9097f9d9b42885c
nerfthisdev <nerfthisdev@users.noreply.github.com>
2025-11-15 12:42:36 +0300

    feat: add models

---
+++ internal/domain/models.go
@@ -0,0 +1,32 @@
+package model
+
+import "time"
+
+type Team struct {
+	Name    string
+	Members []User
+}
+
+type User struct {
+	ID       string
+	Username string
+	TeamName string
+	IsActive bool
+}
+
+type PRStatus string
+
+const (
+	PRStatusOpen    PRStatus = "OPEN"
+	PRStatusMergerd PRStatus = "MERGED"
+)
+
+type PullRequest struct {
+	ID                string
+	Name              string
+	AuthorID          string
+	Status            PRStatus
+	AssignedReviewers []string
+	CreatedAt         time.Time
+	MergedAt          *time.Time
+}

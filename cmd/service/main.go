931b720067bd921b7f211ba1abc078fc1de867c0
nerfthisdev <nerfthisdev@users.noreply.github.com>
2025-11-15 10:55:04 +0300

    feat: add logger init to main

---
+++ cmd/service/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/nerfthisdev/backend-avito/internal/logger"
+
+func main() {
+	cfg := logger.Config{Env: "DEV", Level: ""}
+	logg, err := logger.New(cfg)
+
+	if err != nil {
+		panic("couldnt initialize logger")
+	}
+
+	logg.Info("successfully initialized logger")
+
+}

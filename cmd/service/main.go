9e4cdfd0f527c9d208b5c3e900ada6c655b55742
nerfthisdev <nerfthisdev@users.noreply.github.com>
2025-11-15 11:22:56 +0300

    ci: add lint rules
    fix: config error
    
    ci: add lint rules
    
    format: reformat with gofumpt

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -4,12 +4,11 @@ import "github.com/nerfthisdev/backend-avito/internal/logger"
 
 func main() {
 	cfg := logger.Config{Env: "DEV", Level: ""}
-	logg, err := logger.New(cfg)
 
+	logg, err := logger.New(cfg)
 	if err != nil {
 		panic("couldnt initialize logger")
 	}
 
 	logg.Info("successfully initialized logger")
-
 }
